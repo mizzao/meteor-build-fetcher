bcd4680889fa8af90b737c748adf1391dfd5f10c
Daniel Speichert <daniel@speichert.pl>
2014-08-28 07:42:36 +0200

    0.9.0 compat

--- package.js
+++ package.js
@@ -1,3 +1,9 @@
+Package.describe({
+  summary: 'Downloads remote code to be served as part of a Meteor app',
+  version: '0.2.0',
+  git: 'https://github.com/mizzao/meteor-build-fetcher.git'
+});
+
 Package._transitional_registerBuildPlugin({
     name: "fetcher",
     use: [ 'http' ],
